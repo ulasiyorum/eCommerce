eac09a66a2c9118531bccd7cb3eb6687e1d3fa75
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-05-26 16:11:40 +0300

    Cart Item Model

---
+++ eCommerce-backend/eCommerce-backend/Models/CartItem.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerce_backend.Models;
+
+public class CartItem
+{
+    public int Id { get; set; }
+    [ForeignKey(nameof(User))]
+    public int UserId { get; set; }
+    public User User { get; set; }
+    [ForeignKey(nameof(Movie))]
+    public int MovieId { get; set; }
+    public Movie Movie { get; set; }
+} 
