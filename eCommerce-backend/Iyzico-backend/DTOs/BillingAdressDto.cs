572d239cddfcf7c777f7282648156a1834d600df
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-06-03 16:24:51 +0300

    Dtos

---
+++ eCommerce-backend/Iyzico-backend/DTOs/BillingAdressDto.cs
@@ -0,0 +1,9 @@
+namespace Iyzico_backend.DTOs;
+
+public class BillingAdressDto
+{
+    public required string ContactName { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+    public required string Address { get; set; }
+}
